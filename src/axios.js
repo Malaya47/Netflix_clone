5a9366c35bffc858c07aa2228201c3ce2974b23b
Malaya47 <sidzilegend@gmail.com>
2023-07-30 19:12:00 +0530

    first commit

---
+++ src/axios.js
@@ -0,0 +1,14 @@
+import axios from "axios";
+
+// base url to make requests to the movie database 
+const instance = axios.create({
+    baseURL: "https://api.themoviedb.org/3",
+    
+});
+
+// it will append this to the baseURL
+// instance.get('/foo-bar');
+
+
+export default instance
+;
